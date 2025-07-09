784e6d68e63f0daf81a445d2fd323c0d117187ac
Rafael Rezende <rafael.yanase@gmail.com>
2025-07-09 19:10:09 +0100

    Updated parser initialization

--- srcs/ft_parser.c
+++ srcs/ft_parser.c
@@ -6,7 +6,7 @@
 /*   By: rluis-ya <rluis-ya@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 20:01:47 by rluis-ya          #+#    #+#             */
-/*   Updated: 2025/07/08 14:14:43 by rluis-ya         ###   ########.fr       */
+/*   Updated: 2025/07/09 19:05:29 by rluis-ya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,41 @@
 
 // LEAKS ON LEAKS
 //
+t_vec3	*ft_init_vars(int xvals, int yvals, int zvals, int nrows, int ncols)
+{
+	t_vec3	*matrix;
+	int	*xvals;
+	int	*yvals;
+	int	*zvals;
+	int	size;
+
+	size = nrows * ncols;
+	matrix = (t_vec3 *)malloc(sizeof(t_vec3));
+	if (!matrix)
+		return (NULL);
+	xvals = (int *)malloc(sizeof(int) * size);
+	if (!xvals)
+		return (free(matrix), NULL);
+	yvals = (int *)malloc(sizeof(int) * size);
+	if (!yvals)
+		return (free(matrix), free(xvals), NULL);
+	zvals = (int *)malloc(sizeof(int) * size);
+	if (!zvals)
+	{
+		free(matrix); 
+		return (free(xvals), free(yvals), free(zvals), NULL);
+	}
+	return (matrix);
+}
+
 t_vec3	*ft_parser(char *av)
 {
 	t_vec3	*map;
-	int	*x_vals;
-	int	*y_vals;
-	int	*z_vals;
 	char	**buffer;
 	char	*str;
 	int	fd;
 	int	i;
 
-	map = (t_vec3 *)malloc(sizeof(t_vec3));
-	if (!map)
-		return (NULL);
-	x_vals = (int *) malloc(sizeof(int)*9);
-	if (!x_vals)
-		return (NULL);
-	y_vals = (int *) malloc(sizeof(int)*9);
-	if (!y_vals)
-		return (NULL);
-	z_vals = (int *) malloc(sizeof(int)*9);
-	if (!z_vals)
-		return (NULL);
 	map->x_i = x_vals;
 	map->y_i = y_vals;
 	map->z_i = z_vals;
